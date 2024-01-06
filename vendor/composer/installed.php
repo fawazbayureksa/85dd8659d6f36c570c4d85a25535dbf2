216badfce4712543b83ee8ae9118777888dfe6dc
Fawwaz Bayureksa <fawwaz.bayureksa@singa.id>
2024-01-06 12:20:34 +0700

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7337b459b24bda684a4bba16c860dedea4871d21',
+        'name' => 'fawazbayureksa/85dd8659d6f36c570c4d85a25535dbf2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fawazbayureksa/85dd8659d6f36c570c4d85a25535dbf2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7337b459b24bda684a4bba16c860dedea4871d21',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'dev_requirement' => false,
+        ),
+    ),
+);
